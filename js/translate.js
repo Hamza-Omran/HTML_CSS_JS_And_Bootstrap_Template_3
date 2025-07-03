d28d42522a57ad1acc295c7c79d1cdd0ac5bb45c
Hamza-Omran <139621260+Hamza-Omran@users.noreply.github.com>
2025-07-04 02:22:12 +0300

    Update translate.js

--- js/translate.js
+++ js/translate.js
@@ -1,13 +1,28 @@
 const resources = {
-    en: { translation: await(await fetch('./json/en.json')).json() },
-    ar: { translation: await(await fetch('./json/ar.json')).json() }
+    en: { translation: await(await fetch('../json/en.json')).json() },
+    ar: { translation: await(await fetch('../json/ar.json')).json() }
 };
 
-i18next.init({
-    lng: 'en',
-    debug: true,
-    resources
-}).then(() => updateTexts());
+if(localStorage.getItem("lang")){
+    i18next.init({
+        lng: localStorage.getItem("lang"),
+        debug: true,
+        resources
+    }).then(() => updateTexts());
+
+    // Toggle RTL stylesheet
+    const rtlStyle = document.getElementById("rtl-style");
+    rtlStyle.disabled = localStorage.getItem("lang") !== 'ar';
+
+    document.getElementById("langSelect").value = localStorage.getItem("lang");
+}
+else {
+    i18next.init({
+        lng: 'en',
+        debug: true,
+        resources
+    }).then(() => updateTexts());
+}
 
 function updateTexts() {
     document.querySelectorAll("[data-i18n]").forEach(el => {
@@ -29,7 +44,9 @@ window.changeLang = function (lang) {
         i18next.changeLanguage(lang).then(() => updateTexts());
         document.body.dir = lang === 'ar' ? 'rtl' : 'ltr';
 
+        localStorage.setItem('lang', lang);
+
         // Toggle RTL stylesheet
-    const rtlStyle = document.getElementById("rtl-style");
-    rtlStyle.disabled = lang !== 'ar';
+        const rtlStyle = document.getElementById("rtl-style");
+        rtlStyle.disabled = lang !== 'ar';
     };
